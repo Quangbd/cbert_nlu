5095e02e78aa41932ff9e2a6e78cfb2f2712c361
quangbd <quangbd.hust@gmail.com>
2021-03-24 10:59:19 +0700

    Init the torch version

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <iostream>
 #include <torch/torch.h>
+#include <torch/script.h>
+#include <memory>
 
 #include "tokenizer.h"
 
@@ -31,7 +33,25 @@ int main() {
     std::cout << std::endl;
 
     torch::Tensor tensor = torch::rand({2, 3});
-    std::cout << tensor << std::endl;
+    torch::jit::script::Module module;
+    try {
+        module = torch::jit::load("/Users/quangbd/IdeaProjects"
+                                  "/bert-nlu-training/data/models/snips_convert/model.pt");
+    }
+    catch (const c10::Error &e) {
+        std::cerr << "error loading the model\n";
+        return -1;
+    }
+
+    std::vector<torch::jit::IValue> inputs;
+    auto options = torch::TensorOptions().dtype(torch::kLong);
+    inputs.emplace_back(torch::from_blob(input_ids, {1, 50}, options));
+    inputs.emplace_back(torch::from_blob(segment_ids, {1, 50}, options));
+    inputs.emplace_back(torch::from_blob(input_mask, {1, 50}, options));
+    at::Tensor output = module.forward(inputs).toTuple()->elements()[0].toTensor();
+    output = output.contiguous();
+    std::vector<float> v_output(output.data_ptr<float>(), output.data_ptr<float>() + output.numel());
+    std::cout << '\n';
 
     return 0;
 }
